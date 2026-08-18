c5bd7502bc694554cc17d56d3609fcb90a9d587b
Daniel Bolin <axbolin@iu.edu>
2026-08-18 14:06:24 -0400

    test: fix tests

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,20 +1,10 @@
-import { TestBed } from '@angular/core/testing';
+import { render } from '@testing-library/angular';
 import { App } from './app';
-import { NxWelcome } from './nx-welcome';
+import { appConfig } from './app.config';
 
 describe('App', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [App, NxWelcome],
-    }).compileComponents();
-  });
-
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome envisioning-intelligences-kiosk'
-    );
+  it('renders', async () => {
+    const result = render(App, { providers: appConfig.providers });
+    await expect(result).resolves.toBeDefined();
   });
 });
