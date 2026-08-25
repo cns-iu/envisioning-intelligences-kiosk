931e59b76818cd27ad9b472213c8d55713bb02fb
edlu77 <eddd.lu@gmail.com>
2026-08-25 14:38:29 -0400

    feat: make a component for the logo

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,13 +1,14 @@
 import { Component, input, output } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { Logo } from '../logo/logo';
 
 /**
  *  Header component for the application.
  */
 @Component({
   selector: 'app-header',
-  imports: [RouterModule, MatButtonModule],
+  imports: [RouterModule, MatButtonModule, Logo],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
