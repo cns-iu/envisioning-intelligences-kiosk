3aa38d7ea8a9d1263104558715cb601f3fb649ab
Edward Lu <36934022+edlu77@users.noreply.github.com>
2026-08-25 15:06:41 -0400

    Header (#26)
    * feat: add header component and logo svgs
    
    * feat: add header behavior
    
    * chore: update package-lock
    
    * test: update tests
    
    * chore: updates
    
    * chore: fix formatting for header.html
    
    Co-authored-by: edlu77 <edlu77@users.noreply.github.com>
    
    * feat: make a component for the logo
    
    ---------
    
    Co-authored-by: nx-cloud[bot] <71083854+nx-cloud[bot]@users.noreply.github.com>
    Co-authored-by: edlu77 <edlu77@users.noreply.github.com>

---
+++ src/app/components/logo/logo.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo',
+  imports: [],
+  templateUrl: '../../../../public/assets/wordmark-variable.svg',
+  styleUrl: './logo.scss',
+})
+export class Logo {}
